48708758fc201f0afe760abe0528c9f14a6867ec
ABD <codewmee@gmail.com>
2026-08-08 16:02:01 +0530

    chat included

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
@@ -8,6 +9,10 @@ def home():
     return render_template("main.html")
 
 
+@app.route("/new-issue")
+def chat():
+    return render_template("new-issue.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True,port=9000)
+    app.run(debug=True, port=9000)
